06bffc8480c0f65b1a743d55dd1bd8c005f0c69d
quynh410 <147390551+quynh410@users.noreply.github.com>
2024-08-29 13:03:58 +0700

    Delete session1 directory

--- session1/app/components/Header.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Header() {
-  return (
-    <div style={{width: "100%", height: "60px", background: "black", textAlign: "center", lineHeight: "60px", color:"white"}}>Header</div>
-  )
-}
